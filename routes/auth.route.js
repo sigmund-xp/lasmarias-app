b0ff957b055c1cacbab94672e99d33098e5da300
Mariano <mmbatti@gmail.com>
2025-05-09 14:02:06 -0300

    Correcciones

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { login, register, refreshToken, logout } from '../controllers/auth.controller.js'
+import { login, register, refreshToken, logout, test } from '../controllers/auth.controller.js'
 import { requireRefreshToken } from '../middlewares/requireRefreshToken.js'
 import { bodyLoginValidator, bodyRegisterValidator } from '../middlewares/validatorManager.js'
 
@@ -9,5 +9,6 @@ router.post('/register', bodyRegisterValidator, register)
 router.post('/login', bodyLoginValidator, login)
 router.get('/refresh', requireRefreshToken, refreshToken)
 router.get('/logout', logout)
+router.get('/', test)
 
 export default router
