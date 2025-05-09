b8279356b0e7e0c881ba9f46e8a7d647c06f6409
Mariano <mmbatti@gmail.com>
2025-05-09 16:47:02 -0300

    Revert "Forzado de DB "LMAPP""
    This reverts commit e78c8fba33901e6945d5ece5a53951e0a7bf0e15.

--- database/connectdb.js
+++ database/connectdb.js
@@ -1,9 +1,8 @@
 import mongoose from 'mongoose'
 
 try {
-  const URI_MONGO = process.env.URI_MONGO + '/LMAPP'
+  const URI_MONGO = process.env.URI_MONGO
   await mongoose.connect(URI_MONGO)
-
   console.log(`Conectado a la base ${URI_MONGO}`)
 } catch (error) {
   console.log('Error al conectarse a la base: ' + error)
