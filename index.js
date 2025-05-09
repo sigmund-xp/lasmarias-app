408fd302e30ee4afee35cc84d487021d8815c9c3
Mariano <mmbatti@gmail.com>
2025-05-09 14:13:17 -0300

    Correcciones root

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 // import cors from 'cors'
 
+import { test } from './controllers/auth.controller.js'
 import authRouter from './routes/auth.route.js'
 import linkRouter from './routes/link.route.js'
 
@@ -26,6 +27,8 @@ app.use(express.json())
 app.use(cookieParser())
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/links', linkRouter)
+
+app.get('/', test)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
