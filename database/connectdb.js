e3fcc5d3e035fc512e08c34f85b0c73aceeea51a
Mariano <mmbatti@gmail.com>
2025-05-09 20:39:38 -0300

    Prueba para el uso de la Base LMAPP

--- database/connectdb.js
+++ database/connectdb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 try {
   const URI_MONGO = process.env.URI_MONGO
-  await mongoose.connect(URI_MONGO)
+  await mongoose.connect(URI_MONGO, { dbName: 'LMAPP' })
   console.log(`Conectado a la base ${URI_MONGO}`)
 } catch (error) {
   console.log('Error al conectarse a la base: ' + error)
