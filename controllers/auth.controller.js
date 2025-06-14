9d4004b4347d3605c2bbb9eac49c6d9c4ddacf8f
Mariano <mmbatti@gmail.com>
2025-06-14 20:12:32 -0300

    Log para control

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -65,9 +65,12 @@ export const setUserRole = async (req, res) => {
 
 export const refreshToken = (req, res) => {
   try {
+    console.log(JSON.stringify(req.body))
+    console.log(JSON.stringify(req.uid))
     const { token, expiresIn } = generateToken(req.uid)
     return res.json({ token, expiresIn })
   } catch (error) {
+    console.log(error)
     return res.status(401).json({ error: tokenVerificationErrors(error) })
   }
 }
